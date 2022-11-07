7dbd7bf121c312815c8c1595bd38e35b2851f7ac
zhangzhong <im.zhong@outlook.com>
2022-11-07 09:59:42 +0800

    add list slist stack queue deque heap disjoint_set

--- container/container_of.h
+++ container/container_of.h
@@ -13,6 +13,7 @@
  * @type:   struct type
  * @member: struct member name
  */
-#define container_of(ptr, type, member) ((void *)ptr - offsetof(type, member))
+#define container_of(ptr, type, member)                                        \
+  (type *)((char *)ptr - offsetof(type, member))
 
 #endif // CONTAINER_OF_H
