487c5a6d1bcf660d064a978840b325f1fdb44e93
zhangzhong <im.zhong@outlook.com>
2022-11-07 10:47:01 +0800

    add Makefile to graph

---
+++ graph/main.c
@@ -0,0 +1,5 @@
+// 2022/11/7
+// zhangzhong
+// main.c
+
+int main(int argc, char *argv[]) { return 0; }
---
+++ graph/test.c
@@ -0,0 +1,5 @@
+// 2022/11/7
+// zhangzhong
+// test graph
+
+int main(int argc, char *argv[]) { return 0; }
