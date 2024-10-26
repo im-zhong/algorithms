e9531507291b25d882ead4303ae2d8c6de35d2f0
zhangzhong <im.zhong@outlook.com>
2024-10-26 13:07:26 +0800

    learning permutation

--- enumeration/permutation/permutation.cpp
+++ enumeration/permutation/permutation.cpp
@@ -2,6 +2,7 @@
 // zhangzhong
 
 // generate 1~n permutaion
+#include <algorithm>
 #include <chrono>
 #include <cstdint>
 #include <iostream>
@@ -96,6 +97,82 @@ void PrintPermutationV2(int n, int* A, int cur) {
     }
 }
 
+// 尝试枚举给定的数组，数组P保证是有序的, 无重复的
+void PrintPermutationV3(int n, char* P, char* A, int cur) {
+    // 递归结束条件
+    if (cur == n) {
+        // print the A
+        for (int i = 0; i < n; i++) {
+            std::cout << A[i] << ", ";
+        }
+        std::cout << "\n";
+    } else {
+        // 枚举P中的所有元素
+        for (int i = 0; i < n; i++) {
+
+            bool ok = true;
+            // 检查当前元素P[i] 是否出现在A[0-cur]中
+            for (int j = 0; j < cur; j++) {
+                if (A[j] == P[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                // 这里忘了设置A了
+                A[cur] = P[i];
+                PrintPermutationV3(n, P, A, cur + 1);
+            }
+        }
+    }
+}
+
+void PrintPermutationV4(int n, char* P, char* A, int cur) {
+
+    if (cur == n) {
+        // print the A
+        for (int i = 0; i < n; i++) {
+            std::cout << A[i] << ", ";
+        }
+        std::cout << "\n";
+    } else {
+        for (int i = 0; i < n; i++) {
+            // 重复元素只枚举一次
+            if (i != 0 && P[i] == P[i - 1]) {
+                continue;
+            }
+
+            // 这里就是之前检查已经被使用的元素的地方
+            // 因为可能存在重复的元素
+            // c1 = 所以要检查在A[0:cur]中P[i]出现的次数
+            // c2 = 和P[i]在P中的个数
+            // 只要 c1 < c2， 我们就是okde
+            bool ok = true;
+            int c1 = 0;
+            for (int j = 0; j < cur; j++) {
+                if (A[j] == P[i]) {
+                    c1 += 1;
+                }
+            }
+
+            int c2 = 0;
+            for (int j = 0; j < n; j++) {
+                if (P[j] == P[i]) {
+                    c2 += 1;
+                }
+            }
+
+            ok = c1 < c2;
+
+            if (ok) {
+                A[cur] = P[i];
+                PrintPermutationV4(n, P, A, cur + 1);
+            }
+        }
+    }
+}
+
 // struct Permutation {
 
 //     struct PermutationIterator {
@@ -121,7 +198,7 @@ void PrintPermutationV2(int n, int* A, int cur) {
 
 int main() {
 
-    int n = 10;
+    int n = 4;
 
     // 12455 ms
     auto start = std::chrono::system_clock::now();
@@ -146,4 +223,36 @@ int main() {
         std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
             .count();
     std::cout << duration << " ms" << std::endl;
+
+    char str[4] = "ABC";
+    char permutation[4] = {};
+
+    PrintPermutationV3(3, str, permutation, 0);
+
+    // if we give the repeated sequence, then V3 will be wrong
+    char str1[4] = "AAA";
+    PrintPermutationV3(3, str1, permutation, 0);
+    // 可以看到没有任何输出，因为算法假设P数组中没有任何重复
+    // 所以在枚举时，就会便利A[0-cur]，并设置A[cur]为不重复的元素
+    // 但是P数组中全都是重复的，所以函数无法递归到结束条件达成，所以没有任何输出
+
+    std::cout << "V4 for AAA\n";
+    PrintPermutationV4(3, str1, permutation, 0);
+
+    std::cout << "V4 for AAB\n";
+    char str2[4] = "AAB";
+    PrintPermutationV4(3, str2, permutation, 0);
+
+    // cpp algorithm next permutation
+    // TODO: C++STL中的所有算法，所有容器都得研究明白！
+    // 自己全部实现一遍！但是不考虑范型，只考虑其算法原理
+    // https://en.cppreference.com/w/cpp/algorithm/next_permutation
+
+    std::string s = "aba";
+
+    do {
+        std::cout << s << '\n';
+    } while (std::next_permutation(s.begin(), s.end()));
+
+    std::cout << s << '\n';
 }
